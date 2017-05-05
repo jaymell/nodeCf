3b911636b20eb4c83ac6e451c4cee657dc5c4160
Bob Pace <bob.pace@healthmine.com>
2017-05-04 21:58:28 -0600

    fix indentation and use () => instead of function

--- nodeCf.js
+++ nodeCf.js
@@ -8,52 +8,68 @@ var fs = require('fs');
 // defined in config files
 var cfStackConfigSchema = {
   properties: {
-    name: { 
+    name: {
       type: "string",
       pattern: "^[a-zA-Z0-9\-]+$"
-     },
-    tags: { 
+    },
+    tags: {
       type: "array",
       items: {
         type: "object",
         patternProperties: {
-          "^[a-zA-Z0-9]+$": { type: "string" }
+          "^[a-zA-Z0-9]+$": {
+            type: "string"
+          }
         },
         additionalProperties: false
       },
       additionalItems: false
     },
-    parameters: { 
+    parameters: {
       type: "array",
       items: {
         type: "object",
         patternProperties: {
-          "^[a-zA-Z0-9]+$": { type: "string" }
+          "^[a-zA-Z0-9]+$": {
+            type: "string"
+          }
         },
         additionalProperties: false
       },
       additionalItems: false
     },
-    deps: { type: "array", items: { type: "string" } }
+    deps: {
+      type: "array",
+      items: {
+        type: "string"
+      }
+    }
   },
   required: ["name"]
 };
 
 var envConfigSchema = {
   properties: {
-    application: { type: "string" },
-    account: { 
-      anyOf: [
-          {
-            type: "string",
-            pattern: "^[0-9]+$"             
-          },
-          { type: "integer"}
-        ]
+    application: {
+      type: "string"
     },
-    environment: { type: "string" },
-    infraBucket: { type: "string" },
-    region: { type: "string" },
+    account: {
+      anyOf: [{
+        type: "string",
+        pattern: "^[0-9]+$"
+      }, {
+        type: "integer"
+      }]
+    },
+    environment: {
+      type: "string"
+    },
+    infraBucket: {
+      type: "string"
+    },
+    region: {
+      type: "string"
+    },
   },
   required: ["account", "environment", "application", "infraBucket", "region"]
 };
@@ -66,98 +82,114 @@ function cfStack(spec) {
     environment: spec.environment,
     application: spec.application,
     deps: spec.deps,
-    parameters: _.flattenDeep(_.map(spec.parameters, function(i) {
-                        return _.map(i, (v, k) => ({ ParameterKey: k, ParameterValue: v }));
-                    })),
-    tags: _.flattenDeep(_.map(spec.tags, function(i) {
-                return _.map(i, (v, k) => ({ Key: k, Value: v }));
-              })),
+    parameters: _.flattenDeep(_.map(spec.parameters, (i) => {
+      return _.map(i, (v, k) => ({
+        ParameterKey: k,
+        ParameterValue: v
+      }));
+    })),
+    tags: _.flattenDeep(_.map(spec.tags, (i) => {
+      return _.map(i, (v, k) => ({
+        Key: k,
+        Value: v
+      }));
+    })),
     templateURL: spec.templateURL
   };
 };
 
 // return promise that resolves to true/false or rejects with error
 function bucketExists(cli, bucket) {
-  return cli.headBucket({Bucket: bucket}).promise()
+  return cli.headBucket({
+      Bucket: bucket
+    }).promise()
     .then(() => Promise.resolve(true))
-    .catch(function(e) {
-	  	switch ( e.statusCode ) {
-	  		case 403:
-	  			return Promise.reject(new Error('403: You don\'t have permissions to access this bucket'));
-			case 404:
-	  			return Promise.resolve(false);
-	  		default: 
-	  			throw e;
-	  	}});
+    .catch((e) => {
+      switch (e.statusCode) {
+        case 403:
+          return Promise.reject(new Error('403: You don\'t have permissions to access this bucket'));
+        case 404:
+          return Promise.resolve(false);
+        default:
+          throw e;
+      }
+    });
 }
 
 function createBucket(cli, bucket) {
-	return cli.createBucket({Bucket: bucket}).promise();
+  return cli.createBucket({
+    Bucket: bucket
+  }).promise();
 }
 
 function ensureBucket(cli, bucket) {
-	return bucketExists(cli, bucket)
-           .then(d => d ? Promise.resolve() : createBucket(cli, bucket));
+  return bucketExists(cli, bucket)
+    .then(d => d ? Promise.resolve() : createBucket(cli, bucket));
 }
 
 function s3Upload(cli, bucket, src, dest) {
   console.log(`uploading template ${src} to s3://${bucket}/${dest}`);
   var stream = fs.createReadStream(src);
   return cli.upload({
-      Bucket: bucket,
-      Key: dest,
-      Body: stream
-    }).promise();
+    Bucket: bucket,
+    Key: dest,
+    Body: stream
+  }).promise();
 }
 
 function awsCfStackExists(cli, stackName) {
-  return cli.describeStacks({StackName: stackName}).promise()
-           .then(() => Promise.resolve(true))
-           .catch(function(e) {
-             if (e.message.includes('does not exist')) {
-               return Promise.resolve(false);
-             } 
-             else { 
-              throw e;
-             }
-           });
+  return cli.describeStacks({
+      StackName: stackName
+    })
+    .promise()
+    .then(() => Promise.resolve(true))
+    .catch((e) => {
+      if (e.message.includes('does not exist')) {
+        return Promise.resolve(false);
+      } else {
+        throw e;
+      }
+    });
 }
 
 function createAwsCfStack(cli, params) {
   console.log(`creating cloudformation stack ${params.StackName}`);
   return cli.createStack(params).promise()
-           .then(data => 
-             cli.waitFor('stackCreateComplete', 
-                         {StackName: data.stackId}).promise())
-           .catch(e =>  console.log('Error creating stack: ', e));
+    .then(data =>
+      cli.waitFor('stackCreateComplete', {
+        StackName: data.stackId
+      }).promise())
+    .catch(e => console.log('Error creating stack: ', e));
 }
 
 function ensureAwsCfStack(cli, params) {
   return awsCfStackExists(cli, params.StackName)
-           .then(r => (r ? updateAwsCfStack(cli, params) : createAwsCfStack(cli, params)));
+    .then(r => (r ? updateAwsCfStack(cli, params) : createAwsCfStack(cli, params)));
 }
 
 function updateAwsCfStack(cli, params) {
   console.log(`updating cloudformation stack ${params.StackName}`);
   return cli.updateStack(params).promise()
-           .then(data => 
-             cli.waitFor('stackUpdateComplete', 
-                         {StackName: data.stackId}).promise())
-           .catch(function(e) {
-             switch ( e.message ) {
-               case 'No updates are to be performed.':
-                 return Promise.resolve("No updates are to be performed");
-               default: 
-                 throw e;
-           }});
+    .then(data =>
+      cli.waitFor('stackUpdateComplete', {
+        StackName: data.stackId
+      }).promise())
+    .catch(function(e) {
+      switch (e.message) {
+        case 'No updates are to be performed.':
+          return Promise.resolve("No updates are to be performed");
+        default:
+          throw e;
+      }
+    });
 }
 
 // allows for referencing other variables within the config;
 // recurse until there aren't any more values to be de-templatized:
 function parseConfig(myVars, templateVars) {
-  var myVars = JSON.parse(nunjucks.renderString(JSON.stringify(myVars), templateVars ));
+  var myVars = JSON.parse(nunjucks.renderString(JSON.stringify(myVars), templateVars));
   _.forOwn(myVars, function(v, k) {
-    if ( typeof v === "string" && v.includes('{{') && v.includes('}}'))
+    if (typeof v === "string" && v.includes('{{') && v.includes('}}'))
       myVars = parseConfig(myVars, templateVars);
   });
   return myVars;
@@ -165,12 +197,15 @@ function parseConfig(myVars, templateVars) {
 
 // pass var objects; variables will be
 // env-specific will overwrite any conflicting
-// global vars 
+// global vars
 function loadEnvConfig(env, region, globalVars, envVars, schema) {
   // FIXME: `env` and `region` are handled a bit sloppy, but this
   // is current way to insert them into config, given that they are
   // supplied at runtime:
-  var myVars = _.extend(globalVars, envVars, { environment: env, region: region });
+  var myVars = _.extend(globalVars, envVars, {
+    environment: env,
+    region: region
+  });
   myVars = parseConfig(myVars, JSON.parse(JSON.stringify(myVars)));
 
   if (!(isValidJsonSchema(schema, myVars))) {
@@ -180,7 +215,9 @@ function loadEnvConfig(env, region, globalVars, envVars, schema) {
 }
 
 function isValidJsonSchema(schema, spec) {
-  var ajv = new Ajv({ useDefaults: true });
+  var ajv = new Ajv({
+    useDefaults: true
+  });
   var valid = ajv.compile(schema);
   if (!(valid(spec))) return false;
   return true;
@@ -214,7 +251,7 @@ module.exports = function(AWS, env, region, envVars, globalVars, stackVars, node
   var stackConfig = loadStackConfig(stackVars, envConfig, cfStackConfigSchema);
   // TODO: add validator for nodeCfConfig:
   var nodeCfConfig = nodeCfConfig || defaultNodeCfConfig(envConfig.application,
-      envConfig.environment);
+    envConfig.environment);
   var stacks = _.map(stackConfig.stacks, v => cfStack(v));
 
   return {
@@ -230,18 +267,19 @@ module.exports = function(AWS, env, region, envVars, globalVars, stackVars, node
       var srcDir = nodeCfConfig.localCfTemplateDir;
       var destDir = nodeCfConfig.s3CfTemplateDir;
       return ensureBucket(s3Cli, infraBucket)
-             .then(() => Promise.each(stacks, function(stack) {
-               var src = `${srcDir}/${stack.name}.yml`;
-               var timestamp = new Date().getTime();
-               var dest = `${destDir}/${stack.name}-${timestamp}.yml`;
-               return s3Upload(s3Cli, infraBucket, src, dest)
-                 .then(data => ensureAwsCfStack(cfCli, {
-                   StackName: stack.deployName,
-                   Parameters: stack.parameters,
-                   Tags: stack.tags,
-                   TemplateURL: data.Location}))
-                 .then(() => console.log(`deployed ${stack.deployName}!`));
-             }));
+        .then(() => Promise.each(stacks, function(stack) {
+          var src = `${srcDir}/${stack.name}.yml`;
+          var timestamp = new Date().getTime();
+          var dest = `${destDir}/${stack.name}-${timestamp}.yml`;
+          return s3Upload(s3Cli, infraBucket, src, dest)
+            .then(data => ensureAwsCfStack(cfCli, {
+              StackName: stack.deployName,
+              Parameters: stack.parameters,
+              Tags: stack.tags,
+              TemplateURL: data.Location
+            }))
+            .then(() => console.log(`deployed ${stack.deployName}!`));
+        }));
     }
   };
 };
